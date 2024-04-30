a4c6fd6ce4638c66821d47e6cb4cf7fedd49f27e
Jeremy Spence <148012639+JeremySpence272@users.noreply.github.com>
2024-04-30 17:52:21 -0400

    Update automated-backups.sh

--- automated-backups.sh
+++ automated-backups.sh
@@ -1,7 +1,7 @@
 source_directory="./scripts/"
 backup_directory="./backups/backup_$(date +"%Y%m%d_%H%M%S")"
 
-# Create a backup of the source directory with a timestamp
 cp -a $source_directory $backup_directory
 echo "made a backup of '$source_directory' at '$backup_directory'."
 
+#automated task: create a backup of the script in the script directory into the backup with a timestamp on the folder :)
